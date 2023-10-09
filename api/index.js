8c84286fe0b697dc9fe8d7b0ee33d1a134eeeea4
Tri <16trile@gmail.com>
2023-10-09 16:24:35 -0500

    bug fixes

--- api/index.js
+++ api/index.js
@@ -2,22 +2,30 @@ const express = require("express");
 const apiRouter = express.Router();
 
 apiRouter.get("/ingredients", async (req, res, next) => {
-  const { search } = req.query;
+  try {
+    const { search } = req.query;
 
-  const response = await fetch(
-    `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.API_KEY}&ingredients=${search}&number=10`
-  );
-  const data = await response.json();
-  res.send(data);
+    const response = await fetch(
+      `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.API_KEY}&ingredients=${search}&number=10`
+    );
+    const data = await response.json();
+    res.send(data);
+  } catch (error) {
+    next(error);
+  }
 });
 
 apiRouter.get("/fullRecipe/:id", async (req, res, next) => {
-  const { id } = req.params;
-  const response = await fetch(
-    `https://api.spoonacular.com/recipes/${id}/information?apiKey=${process.env.API_KEY}`
-  );
-  const data = await response.json();
-  res.send(data);
+  try {
+    const { id } = req.params;
+    const response = await fetch(
+      `https://api.spoonacular.com/recipes/${id}/information?apiKey=${process.env.API_KEY}`
+    );
+    const data = await response.json();
+    res.send(data);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = apiRouter;
