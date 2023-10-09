74f398d463893f0374a4341acb6545e01e54b8b6
Tri <16trile@gmail.com>
2023-10-09 16:05:57 -0500

    axios testing

--- src/Search.js
+++ src/Search.js
@@ -9,12 +9,8 @@ const Search = ({ search, setSearch, setRecipes }) => {
           ev.preventDefault();
           const data = await axios.get(`/api/ingredients?search=${search}`, {
             headers: {
-              Accept: "application/json, text/plain, */*",
-              "Accept-Encoding": "gzip, compress, deflate, br",
-              Host: "httpbin.org",
               "User-Agent":
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
-              "X-Amzn-Trace-Id": "Root=1-643d039e-36575b1273b5f45a18b7d9c9",
             },
           });
           console.log(data);
