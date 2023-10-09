7524ffa05fc9fb4bd3f587e2b56f790fd49d2f75
Tri <16trile@gmail.com>
2023-10-09 10:07:36 -0500

    testing express/react with webpack

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const homePage = path.join(__dirname, "index.html");
+app.get("/", (req, res) => res.sendFile(homePage));
+
+const reactApp = path.join(__dirname, "dist/main.js");
+app.get("/dist/main.js", (req, res) => res.sendFile(reactApp));
+
+const reactSourceMap = path.join(__dirname, "dist/main.js.map");
+app.get("/dist/main.js.map", (req, res) => res.sendFile(reactSourceMap));
+
+const styleSheet = path.join(__dirname, "styles.css");
+app.get("/styles.css", (req, res) => res.sendFile(styleSheet));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
